4baf508d2442b7de8098ca3572e52ec6fa4a3df3
Paloma <karpaloma@hotmail.com>
2024-05-23 19:11:10 +0300

    fix:installed modules

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content']
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: ['@nuxt/content', '@nuxtjs/color-mode', "@nuxt/ui", "@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      'DM Sans': '300..700',
+      'JetBrains Mono':true
+    }
+  },
 })
